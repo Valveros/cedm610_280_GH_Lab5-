c97a6a02cecce05441880300fd8b74d4a600aaf3
cedm610 <cedm610@auckland.ac.nz>
2019-08-30 16:45:08 +1200

    updated factorial function

--- cedm610_Lab2_in_Lab5/maths.py
+++ cedm610_Lab2_in_Lab5/maths.py
@@ -42,6 +42,9 @@ def convert_base(num, n):
     return new_num_string
 
 def factorial(number):
-    pass
+    if number == 0:
+        return 1
+    else:
+        return number * factorial(number-1)
     
     
